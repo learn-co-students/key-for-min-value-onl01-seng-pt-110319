a7161d6a2c001e495d1ed682add3fcf85aa612ad
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-12-12 14:06:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 # students = {boys: 15, girls: 34, married: 12, singles: 10}
+# If the method is called and passed an argument of an empty hash, it should return `nil`
+
 def key_for_min_value(students)
   empty = ""
   saved = nil
 students.each do |catagory, number|
-  if  empty == "" || number < empty
+  if  empty == "" || number < empty # Did not understand the logic behind number < empty. Empty is not an integer to compare number against
     saved = catagory
     empty = number
     end
